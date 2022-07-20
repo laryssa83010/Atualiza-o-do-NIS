3107655f406c5cca46a83857c9b1859e87de5589
laryssa rayanne <83220485+laryssa83010@users.noreply.github.com>
2022-07-20 15:29:34 -0300

    Add files via upload

---
+++ atualizaçãodoNIS.py
@@ -0,0 +1,34 @@
+
+
+for c in range(1, 2):
+     c = float(input("Informe o NIS , Apenas o número: "))
+     print('A atualização do NIS {} foi realizada com sucesso !'.format(c))
+
+
+for c in range(1, 2):
+     c = float(input("Digite o CPF ,Apenas o número: "))
+     print('o seu CPF {} foi Atualizado !'.format(c))
+
+
+     Mãe = input("Digite o nome da mãe: ")
+     print('O nome da mãe é:', Mãe, ',foi atualizado')
+
+
+     Pai = input("Digite o nome do Pai: ")
+     print('O nome do seu Pai é:', Pai, ',foi atualizado')
+
+for c in range(1, 2):
+   c = float(input("Digite o número da carteira de trabalho:"))
+   print('O número da carteira de trabalho {} foi Atualizado!'.format(c))
+
+for c in range(1, 2):
+   c = float(input("Digite o número do titulo de eleitor:"))
+   print('O número do titulo de eleitor {} foi Atualizado!'.format(c))
+
+for c in range(1, 2):
+   c = float(input("Digite o número da certidão de nascimento:"))
+   print('o número da certidão de nascimento {} foi Atualizado!'.format(c))
+
+for c in range(1, 2):
+   c = float(input("Digite o número da certidão de casamento,se for casada(o),caso não seja digite 0:"))
+   print('Cadastro Atualizado com sucesso!')
